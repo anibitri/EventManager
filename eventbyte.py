575bbe24458c8cbbef96e908c97281f831138a80
Ani <ani@Anis-MacBook-Air.local>
2024-03-27 01:19:06 +0000

    Changed smpt details

--- eventbyte.py
+++ eventbyte.py
@@ -29,10 +29,11 @@ login_manager.login_view = 'login'
 mail = Mail()
 
 #flask-mail configuration
-app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 2525
-app.config['MAIL_USERNAME'] = '5cd69e80cc5a45'
-app.config['MAIL_PASSWORD'] = 'ee9638ca15fb04'
+#add smtp details
+app.config['MAIL_SERVER']=''
+app.config['MAIL_PORT'] = 1
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
